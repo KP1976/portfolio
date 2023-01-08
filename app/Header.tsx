ea1caa77bf21296b87fe73d480666873e40cd745
Krzysztof Pawłowski <kpawlowski1976@gmail.com>
2023-01-08 11:16:53 +0100

    Add Header component with styles.

---
+++ app/Header.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import styles from '../styles/Header.module.css';
+import Image from 'next/image';
+import headerPic from '../public/header.png';
+
+import { Ubuntu } from '@next/font/google';
+
+const ubuntu = Ubuntu({ weight: '700', subsets: ['latin'] });
+
+export const Header = () => {
+  return (
+    <header className={styles.header}>
+      <Image className={styles['header-image']} src={headerPic} alt="" />
+
+      <h1 className={styles['header-title']}>
+        Chcesz zatrudnić junior front-end developera?
+        <br />
+        <span className={styles['header-title-strong']}>Daj mi szansę!</span>
+      </h1>
+
+      <p className={styles['header-subtitle']}>
+        Od kilku lat programuję w języku{' '}
+        <span className={styles['header-title-strong']}>JavaScript.</span>{' '}
+        Zajmuję się również projektowaniem stron internetowych. Umieściłem kilka
+        swoich prac, które mogą Cię zainteresować.
+      </p>
+
+      <div className={styles.buttons}>
+        <button className={`${styles['cta-button']} ${ubuntu.className}`}>
+          moje prace
+        </button>
+        <button
+          className={`${styles['cta-button']} ${styles['cta-button-second']} ${ubuntu.className}`}
+        >
+          zatrudnij mnie
+        </button>
+      </div>
+    </header>
+  );
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Navigation } from './Navigation';
+import { Header } from './Header';
 
 import styles from '../styles/Home.module.css';
 import { Ubuntu } from '@next/font/google';
@@ -10,6 +11,7 @@ const Home = () => {
   return (
     <div className={`${ubuntu.className} ${styles.container}`}>
       <Navigation />
+      <Header />
     </div>
   );
 };
