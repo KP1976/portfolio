1303f32d94a860fd40b8a0e666ef1aa6d0318162
Krzysztof Pawłowski <kpawlowski1976@gmail.com>
2023-01-14 18:56:14 +0100

    Add single project.

--- app/Project.tsx
+++ app/Project.tsx
@@ -1,10 +1,36 @@
 import React from 'react';
+import Image from 'next/image';
 import styles from '../styles/Project.module.css';
+import todoProject from '../public/todo.jpg';
+import githubIcon from '../public/github.png';
 
 export const Project = () => {
   return (
     <li className={styles.project}>
-      <a href="">1</a>
+      <Image
+        className={styles['project-image']}
+        src={todoProject}
+        alt="ToDo application"
+      />
+
+      <div className={styles['project-title-icon-container']}>
+        <h3 className={styles['project-title']}>Aplikacja TO-DO</h3>
+        <a
+          target="_blank"
+          href="https://github.com/KP1976/TaskManager_frontend"
+          rel="noopener noreferrer"
+        >
+          <Image
+            className={styles['project-icon']}
+            src={githubIcon}
+            alt="Github"
+          />
+        </a>
+      </div>
+
+      <p className={styles['project-description']}>
+        Full-stackowa aplikacja do zarządzania zadaniami.
+      </p>
     </li>
   );
 };
--- app/ProjectsList.tsx
+++ app/ProjectsList.tsx
@@ -6,8 +6,6 @@ export const ProjectsList = () => {
   return (
     <ul className={styles['projects-list']}>
       <Project />
-      <Project />
-      <Project />
     </ul>
   );
 };
