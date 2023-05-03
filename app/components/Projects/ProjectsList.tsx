3fd238cd68cbc4d8f5843cb207d5c0c8d534e8f3
Krzysztof Pawłowski <kpawlowski1976@gmail.com>
2023-05-03 13:59:03 +0200

    Add local database with projects.

--- app/components/Projects/ProjectsList.tsx
+++ app/components/Projects/ProjectsList.tsx
@@ -1,34 +1,33 @@
 import React from 'react';
-import bakeryProject from '../../../public/cukiernia.jpg';
-import tilesProject from '../../../public/kafelki.jpg';
-import todoProject from '../../../public/todo.jpg';
 import { Project } from './Project';
+import { projects } from '../../database/projects';
 
 import styles from '../../../styles/ProjectsList.module.css';
 
 export const ProjectsList = () => {
   return (
     <ul className={styles['projects-list']}>
-      <Project
-        image={todoProject}
-        projectTitle="Aplikacja TO-DO"
-        projectDescription="Full-stackowa aplikacja do zarządzania zadaniami."
-        projectGithubLink="https://github.com/KP1976/TaskManager_frontend"
-      />
-      <Project
-        image={tilesProject}
-        projectTitle="Gra w kafelki"
-        projectDescription="Gra, w której odkrywamy kolejno dwa pasujące kolorami do siebie kafelki."
-        projectGithubLink="https://github.com/KP1976/kafelki"
-        projectLink="https://kp1976.github.io/kafelki"
-      />
-      <Project
-        image={bakeryProject}
-        projectTitle="Cukiernia"
-        projectDescription="Sklep internetowy, w którym możemy zakupić różnego rodzaju ciastka."
-        projectGithubLink="https://github.com/KP1976/BAKERY_SHOP"
-        projectLink="https://kp1976.github.io/BAKERY_SHOP"
-      />
+      {projects.map(
+        ({
+          id,
+          image,
+          projectTitle,
+          projectDescription,
+          projectGithubLink,
+          projectLink,
+        }) => {
+          return (
+            <Project
+              key={id}
+              image={image}
+              projectTitle={projectTitle}
+              projectDescription={projectDescription}
+              projectGithubLink={projectGithubLink}
+              projectLink={projectLink}
+            />
+          );
+        }
+      )}
     </ul>
   );
 };
