bc7787a49f552aad50fc032719b3d3e40e4fb808
Krzysztof Pawłowski <kpawlowski1976@gmail.com>
2023-06-11 12:08:53 +0200

    Fix logo paths... again.

--- components/AboutMe/TechnologyList.tsx
+++ components/AboutMe/TechnologyList.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import HTMLlogo from '@public/HTML5_logo.svg';
 import CSSlogo from '@public/CSS3_logo.svg';
 import JSlogo from '@public/JavaScript_logo.svg';
-import SASSlogo from '@public/Sass_logo.svg';
+import SASSlogo from '@public/Sass_Logo.svg';
 import Reactlogo from '@public/React_logo.svg';
 import NODEJSlogo from '@public/NodeJs_logo.svg';
 import MYSQLlogo from '@public/mysql-official.svg';
