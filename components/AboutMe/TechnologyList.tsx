48a67ff36c139e8bd7e97b96427230a5c07cf2db
Krzysztof Pawłowski <kpawlowski1976@gmail.com>
2023-09-15 14:59:23 +0200

    Fix TechnologyList component.

--- components/AboutMe/TechnologyList.tsx
+++ components/AboutMe/TechnologyList.tsx
@@ -1,15 +1,5 @@
 import React from 'react';
 
-<<<<<<< Updated upstream
-import HTMLlogo from '@public/html5_logo.svg';
-import CSSlogo from '@public/css3_logo.svg';
-import JSlogo from '@public/JavaScript_logo.svg';
-import SASSlogo from '@public/Sass_logo.svg';
-import Reactlogo from '@public/React_logo.svg';
-import NODEJSlogo from '@public/NodeJs_logo.svg';
-import MYSQLlogo from '@public/mysql-official.svg';
-import NEXTJSlogo from '@public/Next.js_logo.svg';
-=======
 import HTMLlogo from '@public/tech-logos/HTML5_logo.png';
 import CSSlogo from '@public/tech-logos/CSS3_logo.png';
 import JSlogo from '@public/tech-logos/JavaScript_logo.png';
@@ -18,7 +8,6 @@ import Reactlogo from '@public/tech-logos/React_logo.png';
 import NODEJSlogo from '@public/tech-logos/NodeJs_logo.png';
 import MYSQLlogo from '@public/tech-logos/mysql-official.png';
 import NEXTJSlogo from '@public/tech-logos/Next.png';
->>>>>>> Stashed changes
 
 import { TechnologyItem } from './TechnologyItem';
 
